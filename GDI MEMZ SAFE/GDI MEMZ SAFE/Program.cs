8306d7c990e1293b45f86a29ec9f5f1af03fce2d
MATTIAloyoutuber <164758246+MATTIAloyoutuber@users.noreply.github.com>
2024-04-14 21:37:51 +0200

    UN MISTO TRA MEMZ E CHILLED WINDOWS (SAFE)

---
+++ GDI MEMZ SAFE/GDI MEMZ SAFE/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ScreenBlinker
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool PatBlt(IntPtr hdc, int nXLeft, int nYLeft, int nWidth, int nHeight, uint dwRop);
+
+        static void Main(string[] args)
+        {
+            int screenWidth = 1920;
+            int screenHeight = 1080;
+
+            IntPtr desktopPtr = GetDC(IntPtr.Zero);
+            IntPtr hdc = desktopPtr;
+
+            while (true)
+            {
+                PatBlt(hdc, 0, 0, screenWidth, screenHeight, 0x005A0049);
+                Thread.Sleep(100);
+                PatBlt(hdc, 0, 0, screenWidth, screenHeight, 0x005A0049);
+                Thread.Sleep(100);
+            }
+
+            ReleaseDC(IntPtr.Zero, desktopPtr);
+        }
+    }
+}
